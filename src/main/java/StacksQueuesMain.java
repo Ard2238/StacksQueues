9ea7b7336d1e25c9092250e2425e3f24db4045d1
Ard2238 <abhishekrdas28@gmail.com>
2020-10-17 07:40:48 +0530

    Added Code to push data onto the stack

--- src/main/java/StacksQueuesMain.java
+++ src/main/java/StacksQueuesMain.java
@@ -1,5 +1,15 @@
 public class StacksQueuesMain {
+    StacksQueues stack;
+
+    public StacksQueuesMain(){
+        stack = new StacksQueues();
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to the Stacks and Queues Program");
+
+        StacksQueuesMain sqm = new StacksQueuesMain();
+        sqm.stack.pushData(70);
+        sqm.stack.pushData(30);
+        sqm.stack.pushData(56);
     }
 }
